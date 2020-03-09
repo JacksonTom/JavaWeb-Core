75a307bcef15f04014a51b76c4464e1fe56c9a50
JacksonTom <JacksonTom@qq.com>
2020-03-09 16:49:42 +0800

    javascript高级

--- day01_基础加强/src/cn/itcast/test/CalculatorTest.java
+++ day01_基础加强/src/cn/itcast/test/CalculatorTest.java
@@ -1,10 +1,7 @@
 package cn.itcast.test;
 
 import cn.itcast.junit.Calculator;
-import org.junit.After;
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.*;
 
 public class CalculatorTest {
     /**
